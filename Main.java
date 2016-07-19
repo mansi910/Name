f9b517984545deb34a59bf4430fa399c9adb60d5
mansi910 <mansi09101993@gmail.com>
2016-07-19 16:13:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
-public class Main {
-   public static void main(String args[]) {
-      String str = "this is Java";
-      System.out.println(removeCharAt(str, 3));
-   }
-   public static String removeCharAt(String s, int pos) {
-     
-     
-      return s.substring(0, pos) + s.substring(pos + 1);
+class PrimeNumbers
+{
+   public static void main (String[] args)
+   {		
+       int i =0;
+       int num =0;
+       //Empty String
+       String  primeNumbers = "";
+
+       for (i = 1; i <= 100; i++)         
+       { 		  	  
+          int counter=0; 	  
+          for(num =i; num>=1; num--)
+	  {
+             if(i%num==0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ==2)
+	  {
+	     //Appended the Prime number to the String
+	     primeNumbers = primeNumbers + i + " ";
+	  }	
+       }	
+       System.out.println("Prime numbers from 1 to 100 are :");
+       System.out.println(primeNumbers);
    }
 }
