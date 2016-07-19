78f71b58ad85214a8435e8ccf93ff97230be841c
mansi910 <mansi09101993@gmail.com>
2016-07-19 14:08:48 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,6 +4,8 @@ public class Main {
       System.out.println(removeCharAt(str, 3));
    }
    public static String removeCharAt(String s, int pos) {
+      System.out.println("hello");
+      System.out.println("hello");
       return s.substring(0, pos) + s.substring(pos + 1);
    }
 }
